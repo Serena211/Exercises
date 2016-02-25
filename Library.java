313b5b91564b0f19b4cd1183de946613f6f64b91
Serena <sunflowerxuanyu@gmail.com>
2016-02-25 17:36:39 -0500

    Update Library.java
    edit format.

--- Library.java
+++ Library.java
@@ -13,31 +13,33 @@ public class Library {
   public void getFinishedBooks(HashMap<String,Boolean> library) {
     if (library.size() < 1) {
     	System.out.println("Error! No books!");
-    } else {
-    	for(String book: library.keySet()) {
-        if (library.get(book)) {
-        	System.out.println("This book " + book + " has finished!");
-        }
+    } 
+    for(String book: library.keySet()) {
+      if (library.get(book)) {
+        System.out.println("This book " + book + " has finished!");
       }
     }
+    
   }
   public void getUnFinishedBooks(HashMap<String,Boolean> library) {
     if (library.size() < 1) {
     	System.out.println("Error! No books!");
-    } else {
-    	for(String book: library.keySet()) {
-        if (!library.get(book)) {
-        	System.out.println("This book " + book + " has not finished!");
-        }
+    }
+    for(String book: library.keySet()) {
+      if (!library.get(book)) {
+        System.out.println("This book " + book + " has not finished!");
       }
     }
+    
   }
   public static void main(String[] args) {
   	HashMap<String,Boolean> myBooks = new HashMap<String,Boolean>();
+    
     myBooks.put("Road Down The Funnel",true);
     myBooks.put("Rat: A Biology",false);
     myBooks.put("TimeIn",true);
     myBooks.put("3D Food Printing",false);
+    
     Library myLibrary = new Library();
     myLibrary.getFinishedBooks(myBooks);
     myLibrary.getUnFinishedBooks(myBooks);
