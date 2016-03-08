dcd541e31a4c15ba958ab3e79f05f1336b06f020
Serena <sunflowerxuanyu@gmail.com>
2016-03-08 14:56:53 -0500

    Added files via upload

---
+++ Java/Stanford_CS106/FactorialExample.java
@@ -0,0 +1,19 @@
+import acm.program.ConsoleProgram;
+
+
+public class FactorialExample extends ConsoleProgram{
+	private static final int MAX_NUM = 10;
+	public void run() {
+		for (int i = 0; i < MAX_NUM; i++) {
+			println(i + " is " + factorial(i));
+		}
+	}
+	private int factorial(int n) {
+		int rsl = 1;
+		for (int i = 0; i < n; i++) {
+			rsl += i;
+		}
+		return rsl;
+	}
+
+}
