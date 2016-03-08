d1207cd04decb962299497fb82e8b7da663c6134
Serena <sunflowerxuanyu@gmail.com>
2016-03-08 15:04:07 -0500

    Update FactorialExample.java

--- Java/Stanford_CS106/FactorialExample.java
+++ Java/Stanford_CS106/FactorialExample.java
@@ -5,13 +5,13 @@ public class FactorialExample extends ConsoleProgram{
 	private static final int MAX_NUM = 10;
 	public void run() {
 		for (int i = 0; i < MAX_NUM; i++) {
-			println(i + " is " + factorial(i));
+			println(i + "! = " + factorial(i));
 		}
 	}
 	private int factorial(int n) {
 		int rsl = 1;
-		for (int i = 0; i < n; i++) {
-			rsl += i;
+		for (int i = 1; i <= n; i++) {
+			rsl *= i;
 		}
 		return rsl;
 	}
