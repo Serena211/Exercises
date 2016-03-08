b13092bfd530b6049659e4a3b9acf15f5998d42b
Serena <sunflowerxuanyu@gmail.com>
2016-03-07 21:16:22 -0500

    format

--- Java/Stanford_CS106/Checkerboard.java
+++ Java/Stanford_CS106/Checkerboard.java
@@ -7,13 +7,13 @@ public class Checkerboard extends GraphicsProgram{
 	private static final int NCOLUNMS = 8;
 	//draw the checkerboard
 	public void run() {
-		int sqSize = getHeight()/8;
+		int sqSize = getHeight() / NROWS;
 		for (int i = 0; i < NROWS; i++) {
 			for (int j = 0; j < NCOLUNMS; j++) {
 				int x = j * sqSize;
 				int y = i * sqSize;
-				GRect sq = new GRect(x,y,sqSize,sqSize);
-				sq.setFilled(((i+j)%2) != 0);
+				GRect sq = new GRect(x, y, sqSize, sqSize);
+				sq.setFilled(((i + j) % 2) != 0);
 				add(sq);
 			}
 		}
