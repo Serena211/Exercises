b3f191aa98ba63667576d4b29ed860c588c8ac43
Serena <sunflowerxuanyu@gmail.com>
2016-03-10 23:49:46 -0500

    Update MouseTracker.java
    add new mouse listeners.

--- Java/Stanford_CS106/MouseTracker.java
+++ Java/Stanford_CS106/MouseTracker.java
@@ -5,15 +5,26 @@ import acm.program.GraphicsProgram;
 
 
 public class MouseTracker extends GraphicsProgram{
+	
 	public void run() {
-		label = new GLabel("");
+		label = new GLabel("Please Move or Click \t your mouse to get the location! ");
 		label.setFont("Helvetica");
-		add(label,getHeight()/3 ,getHeight()/4);
+		add(label,getHeight()/4 ,getHeight()/3);
 		
 		addMouseListeners();
 	}
+	
 	public void mouseMoved(MouseEvent e) {
 		label.setLabel("Position: " + e.getX() + " , " + e.getY());
 	}
+	
+	public void mouseClicked(MouseEvent e) {
+		label.setLabel("Mouse Position is " + e.getX() + " , " + e.getY() +".");
+	}
+	
+	public void mouseExited(MouseEvent e) {
+		label.setLabel("Please Move or Click your mouse to get the location! ");
+	}
+
 	private GLabel label;
 }
